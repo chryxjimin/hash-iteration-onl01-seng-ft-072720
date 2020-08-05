33d46e6ecc56a4d8b47816b7b990739e9c3fc688
Christie Yoon <cleex1999@gmail.com>
2020-08-05 16:15:09 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,7 +12,8 @@ def happy_birthday(birthday_kids)
 
 def age_appropriate_birthday
   birthday_kids.each do |kids_name, age|
-    if age <=12 
+    if age <=12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
+end
 end
